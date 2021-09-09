790a48a7ead697c09e90fb9ddb1b255c1548cbb2
Dev Nirwal <devnirwal16@gmail.com>
2021-09-09 12:35:55 +0530

    Time: 2 ms (72.16%), Space: 39.4 MB (40.41%)

--- remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -4,6 +4,6 @@ class Solution {
         if(!(s.contains(part))){
            return s;
         }
-           return removeOccurrences(s.substring(0,s.indexOf(part)) + s.substring(s.indexOf(part)+part.length(),s.length()),part);
+           return removeOccurrences(s.substring(0, s.indexOf(part)) + s.substring(s.indexOf(part)+part.length(),s.length()),part);
     }
 }
