6832a4acafdc8d5317513cf99912fd6439542bdf
Dev Nirwal <devnirwal16@gmail.com>
2021-09-14 15:30:14 +0530

    Time: 2 ms (39.07%), Space: 37.9 MB (25.27%)

--- reverse-integer/reverse-integer.java
+++ reverse-integer/reverse-integer.java
@@ -1,13 +1,11 @@
 class Solution {
     public int reverse(int x) {
-        
-        long result = 0;
+        long result = 0;        
         while(x!=0){
             result = result*10 + (x%10);
-            if(result > Integer.MAX_VALUE) return 0;
-            if(result < Integer.MIN_VALUE) return 0;
             x = x /10;
         }
+        if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) return 0;
         return (int) result;
     }
 }
