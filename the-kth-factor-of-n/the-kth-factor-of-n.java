5a09f8193ae912751c04b6d69a296b461aab3a20
Dev Nirwal <devnirwal16@gmail.com>
2021-09-16 14:26:12 +0530

    Time: 2 ms (25.80%), Space: 34.20 MB (37.6 MB%)

--- the-kth-factor-of-n/the-kth-factor-of-n.java
+++ the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,12 +1,8 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        int counter= 0;
         for(int i = 1;i<=n;i++){
-            if(n%i==0){
-                counter++;
-            }
-            if(counter==k){
-                return i;
+            if(n%i==0 && --k==0){
+               return i;
             }
         }
         return -1;
