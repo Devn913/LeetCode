27dc7f4a31cfac5549b5cb6b393b77fcb73d403d
Dev Nirwal <devnirwal16@gmail.com>
2021-09-19 20:58:17 +0530

    Time: 11 ms (14.12%), Space: 39.3 MB (48.63%) - LeetHub

--- maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
+++ maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -1,27 +1,47 @@
-class Solution {   
-    public int maxProductDifference(int[] arr) {
-        int i = 0;
-        int j = -1;
-        int k = Integer.MAX_VALUE;
-        int l = Integer.MAX_VALUE;
+class Solution {  
+    
+    void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    int partation(int arr[], int si, int ei){
         int counter = 0;
-        while(counter<arr.length){
-            if(arr[counter]>= i){
-                j = i;
-                i = arr[counter];
-            }else if(arr[counter]>j){
-                j = arr[counter];
+        for(int i = si+1;i<=ei;i++){
+            if(arr[si]>arr[i]){
+                counter++;
             }
-            
-            if(arr[counter]<=k){
-                l = k;
-                k = arr[counter];
-            }else if(arr[counter]<l){
-                l = arr[counter];
-            }
-            
-            counter++;
         }
-        return i*j - k*l;
+        swap(arr,si,si+counter);
+        int i = si;
+        int j = ei;
+        while(i<si+counter && si+counter<j){
+            if(arr[i]>=arr[si+counter]){
+                if(arr[j]<arr[si+counter]){
+                    swap(arr,i,j);
+                    i++;
+                }
+                j--;
+            }else{
+                if(arr[j]>=arr[si+counter]){
+                    j--;
+                }
+                i++;
+            }
+        }
+        return si+counter;
+    
+    }
+    public void quickSort(int[] arr, int si, int ei){
+        if(si>=ei){
+            return;
+        }
+        int c = partation(arr,si,ei);
+        quickSort(arr,si,c-1);
+        quickSort(arr,c+1,ei);
+    }
+    public int maxProductDifference(int[] arr) {
+        quickSort(arr,0,arr.length-1);
+        return arr[arr.length -1]* arr[arr.length -2] - arr[0] * arr[1];
     }
 }
