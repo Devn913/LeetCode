342fd0b0e0e87c9cb123c6a53c7286f2d33f01ca
Dev Nirwal <devnirwal16@gmail.com>
2021-09-21 22:15:28 +0530

    Time: 79 ms (21.65%), Space: 43.5 MB (35.47%)

--- count-primes/count-primes.java
+++ count-primes/count-primes.java
@@ -1,21 +1,19 @@
 class Solution {  
     public int countPrimes(int n) {
+        if(n<=1) return 0;
         boolean[] notPrime = new boolean[n];
         int res = 0;
         for(int i = 2;i< Math.sqrt(n);i++){
             if(notPrime[i]== false){
                 for(int j = i*i;j<n;j=j+i){
-                    notPrime[j] = true;
+                     if(!(notPrime[j])){
+                         notPrime[j] = true;
+                         res++;
+                     }
+                    
                 }
             }
         }
-        int counter = 2;
-        while(counter<n){
-            if(!notPrime[counter]){
-                res++;
-            }
-            counter++;
-        }
-        return res;
+        return n-res-2;
     }
 }
