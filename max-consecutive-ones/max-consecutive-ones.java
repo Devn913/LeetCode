fd80b63f2bf381504a253477baed762e755a290e
Dev Nirwal <devnirwal16@gmail.com>
2021-09-21 16:03:05 +0530

    Time: 1 ms (100.00%), Space: 4 MB (63.21%)

--- max-consecutive-ones/max-consecutive-ones.java
+++ max-consecutive-ones/max-consecutive-ones.java
@@ -1,17 +1,15 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] arr) {
         int result = 0;
-        int intermediate = 0;
+        short intermediate = 0;
         for(int i = 0;i<arr.length;i++){
-
             if(arr[i] == 1){
                 intermediate++;
-            }
-            if(arr[i] != 1){
+            }else{
                 if(result<intermediate) result = intermediate;
                 intermediate = 0;
             }
         }
-        return result>intermediate ? result : intermediate ;
+        return Math.max(result,intermediate) ;
     }
 }
