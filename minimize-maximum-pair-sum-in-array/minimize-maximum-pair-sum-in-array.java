6027dd419019e014d5ce8a84b4ecb27d271a5b51
Dev Nirwal <devnirwal16@gmail.com>
2021-09-21 12:08:36 +0530

    Time: 48 ms (92.64%), Space: 55.4 MB (41.27%)

--- minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.java
+++ minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.java
@@ -1,57 +1,16 @@
-class Solution {
-    public void swap(int[] arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    
-    public int partation(int arr[], int si, int ei){
-        int counter = 0;
-        for(int i = si+1;i<=ei;i++){
-            if(arr[si]>arr[i]){
-                counter++;
-            }
-        }
-        swap(arr,si,si+counter);
-        int i = si;
-        int j = ei;
-        while(i<si+counter && si+counter<j){
-            if(arr[i]>=arr[si+counter]){
-                if(arr[j]<arr[si+counter]){
-                    swap(arr,i,j);
-                    i++;
-                }
-                j--;
-            }else{
-                if(arr[j]>=arr[si+counter]){
-                    j--;
-                }
-                i++;
-            }
-        }
-        return si+counter;
-    }
-    public void quickSort(int[] arr, int si, int ei){
-        if(si>=ei){
-            return;
-        }
-        int c = partation(arr,si,ei);
-        quickSort(arr,si,c-1);
-        quickSort(arr,c+1,ei);
-    }
-    
+class Solution {   
         
     public int minPairSum(int[] arr) {
-        quickSort(arr,0,arr.length-1);
+        Arrays.sort(arr);
         int result = 0;
-        int min =0;
-        int max = arr.length -1;
+        int min = 0;
+        int max = arr.length-1;
         while(min<=max){
             if(result<arr[min] + arr[max]){
                 result = arr[min] + arr[max];
             }
             min++;
-            max --;
+            max--;
         }
         return result;
         
