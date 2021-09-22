e2b960064eb596ec7d1ae016de4642ffde975caa
Dev Nirwal <devnirwal16@gmail.com>
2021-09-23 02:33:09 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (77.26%)

--- sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
+++ sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.java
@@ -1,13 +1,10 @@
 class Solution {
     public int arraySign(int[] nums) {
-        short negatives = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i]<0){
-                negatives++;
-            }
-            if(nums[i] ==0) return 0;
+        int sign = 1;
+        for(short i = 0;i<nums.length;i++){
+            if(nums[i]<0) sign = -sign;
+            else if(nums[i] ==0) return 0;
         }
-        if(negatives%2==0) return 1;
-        return -1;
+        return sign;
     }
 }
