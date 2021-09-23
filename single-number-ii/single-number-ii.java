529f0d87bb44dfa02c392b4a69ecead508ed6985
Dev Nirwal <devnirwal16@gmail.com>
2021-09-23 13:06:53 +0530

    Time: 1 ms (82.69%), Space: 38.7 MB (53.77%)

--- single-number-ii/single-number-ii.java
+++ single-number-ii/single-number-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
         if(nums.length==1) return nums[0];
+        int len = nums.length;
         Arrays.sort(nums);
-        int counter = 1;
         if(nums[1]!=nums[0]) return nums[0];
-        else if(nums[nums.length-1] != nums[nums.length-2]) return nums[nums.length-1];
+        else if(nums[len-1] != nums[len-2]) return nums[len-1];
         else{
-            while(counter<nums.length){
+            int counter = 1;
+            while(counter<len){
                 if(!(nums[counter-1] == nums[counter])){
                     return nums[counter-1];
                 }
                 counter = counter + 3;
             }
         }
-        return counter-1;
+        return 0;
     }
 }
