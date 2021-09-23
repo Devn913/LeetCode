610173c87cf23359bc1280b010d7b35f4a7383f2
Dev Nirwal <devnirwal16@gmail.com>
2021-09-23 20:17:51 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (55%)

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -2,8 +2,8 @@ class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
         int sum = 0;
-        for(int i = 0;i<n;i++){
-            sum = sum+nums[i];
+        for(int num: nums){
+            sum +=num;
         }
         return (n+1)*(n)/2 - sum;
     }
