f484c16a178e911fdfa2d797ad82a6f76b639aed
Dev Nirwal <devnirwal16@gmail.com>
2021-12-05 16:57:04 +0530

    Time: 5 ms (98.44%), Space: 43.3 MB (99.75%) - LeetHub

---
+++ sort-list/sort-list.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        if(l1.val>l2.val){
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+        ListNode res = l1;
+        ListNode temp  = l1;
+        l1 = l1.next;
+        while(l2!=null){
+            if(l1 == null){
+                temp.next = l2;
+                break;
+            }
+            if(l1.val>l2.val){
+                temp.next = l2;
+                l2 =l2.next;
+                temp = temp.next;
+                temp.next = l1;
+            }else{
+                temp = l1;
+                l1 = l1.next;
+            }
+        }
+        return res;   
+    }
+    
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next !=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        return mergeTwoLists(l1,l2);
+    }
+}
