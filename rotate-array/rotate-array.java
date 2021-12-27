c355ea1f7e4a2cc2b89a49c1da41816bfdc52163
Dev Nirwal <devnirwal16@gmail.com>
2021-12-27 15:14:07 +0530

    Time: 0 ms (100.00%), Space: 55.7 MB (97.69%) - LeetHub

--- rotate-array/rotate-array.java
+++ rotate-array/rotate-array.java
@@ -1,24 +1,21 @@
 class Solution {
-    public void swap(int[] arr, int i,int j){
-        int temp =arr[i];
+    public void swap(int[] arr,int i, int j){
+        int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    public void reverse(int[] arr, int start, int end){
+        while(start<end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
+    }
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k%n;
-        k = n-k;
-        int[] arr =  new int[k];
-        int i = 0;
-        for(;i<k;i++) arr[i] = nums[i];
-        i=0;
-        for(;i<n - k ;i++){
-            swap(nums,i,i+k);
-        }
-        for(int num: arr){
-            nums[i] = num;
-            i++;
-        }
-        
+        int len =nums.length;
+        k = k%len;
+        reverse(nums,0,len-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,len-1);
     }
 }
