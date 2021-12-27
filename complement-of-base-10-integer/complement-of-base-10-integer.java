f8edc5153b04d865a51d3ca5aad2b8249925b235
Dev Nirwal <devnirwal16@gmail.com>
2021-12-27 12:10:34 +0530

    Time: 0 ms (100.00%), Space: 35.9 MB (41.94%) - LeetHub

---
+++ complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n==0) return 1;
+        int res = 0;
+        int c = 0;
+        while(n!=0){
+            if(n%2==0) res += (int)Math.pow(2,c);
+            c++;
+            n = n/2;
+        }
+        return res;
+    }
+}
