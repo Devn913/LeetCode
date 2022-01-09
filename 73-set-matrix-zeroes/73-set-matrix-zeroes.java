a5cfb37dd115c2c37f77c75064407cd30c66fbe5
Dev Nirwal <devnirwal16@gmail.com>
2022-01-09 15:31:52 +0530

    Time: 2 ms (48.35%), Space: 50.6 MB (19.93%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,23 +1,53 @@
 class Solution {
     
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> rows = new HashSet<>(matrix.length);
-        HashSet<Integer> cols = new HashSet<>(matrix[0].length);
-        for(int i =0;i<matrix.length;i++){
-            for(int j = 0;j<matrix[0].length;j++){
+        Boolean isRow = false;
+        Boolean isCol = false;
+        for(int i = 0;i<matrix[0].length;i++){
+            if(matrix[0][i]==0){
+                isRow = true;
+                break;
+            }
+        }
+        for(int i = 0;i<matrix.length;i++){
+            if(matrix[i][0] == 0){
+                isCol = true;
+                break;
+            }
+        }
+        for(int i = 1;i<matrix.length;i++){
+            for(int j = 1;j<matrix[0].length;j++){
                 if(matrix[i][j] == 0){
-                    rows.add(i);
-                    cols.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i =0;i<matrix.length;i++){
-            for(int j = 0;j<matrix[0].length;j++){
-                if(rows.contains(i) || cols.contains(j)){
+        for(int i = 1;i<matrix[0].length;i++){
+            if(matrix[0][i] == 0){
+                for(int j = 1;j<matrix.length;j++){
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+        for(int i = 1;i<matrix.length;i++){
+            if(matrix[i][0] == 0){
+                for(int j = 1;j<matrix[0].length;j++){
                     matrix[i][j] = 0;
                 }
             }
         }
+        if(isRow){
+            for(int i = 0;i<matrix[0].length;i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if(isCol){
+            for(int i = 0;i<matrix.length;i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
         
     }
 }
