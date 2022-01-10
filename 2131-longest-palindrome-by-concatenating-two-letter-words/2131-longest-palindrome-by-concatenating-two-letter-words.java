92d3ec693f0eea1a30927fb8af83518586911f5a
Dev Nirwal <devnirwal16@gmail.com>
2022-01-10 20:27:33 +0530

    Time: 171 ms (20.00%), Space: 122.4 MB (20.00%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    public String reverse(String s){
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        return sb.reverse().toString();
+    }
+    
+    
+    public int longestPalindrome(String[] words) {
+        HashMap<String,Integer> map = new HashMap<>();
+        int res = 0;
+        int unpaired = 0;
+        for(String s: words){
+            if(s.charAt(0) == s.charAt(1)){
+                if(map.containsKey(s) && map.get(s)>0){
+                    unpaired--;
+                    res+=4;
+                    map.put(s,map.getOrDefault(s,1)-1);
+                }else{
+                    unpaired++;
+                    map.put(s, map.getOrDefault(s,0) + 1);
+                }
+            }else{
+                String rev  = reverse(s);
+                if(map.containsKey(rev) && map.getOrDefault(rev,0) > 0){
+                    res+=4;
+                    map.put(rev,map.getOrDefault(rev,1) -1);
+                }else{
+                    map.put(s,map.getOrDefault(s,0)+1);
+                }
+            }
+        }
+        if(unpaired>0) res+=2;
+        return res;
+    }
+}
