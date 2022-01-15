912b073acc6f581d7f466b2095d7a698bc32fa4a
Dev Nirwal <devnirwal16@gmail.com>
2022-01-15 14:50:05 +0530

    Time: 16 ms (11.45%), Space: 51.9 MB (8.64%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,33 +1,61 @@
 class Solution {
-    public boolean helper(int[][] matrix, int current, int target){
-        if(current>=matrix.length|| current>=matrix[0].length) return false;
-        if(matrix[current][current]>target) return false;
-        int low = current;
-        int high = matrix[current].length -1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(matrix[current][mid]>target){
-                high = mid -1;
-            }else if(matrix[current][mid]<target){
-                low = mid+1;
-            }else{
-                return true;
-            }
-        }
-        low = current;
-        high = matrix.length - 1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(matrix[mid][current]>target){
-                high = mid -1;
-            }else if(matrix[mid][current]<target){
-                low = mid+1;
-            }else return true;
-        }
-        return helper(matrix,current+1,target);
-    }
+    // public boolean helper(int[][] matrix, int current, int target){
+    //     if(current>=matrix.length|| current>=matrix[0].length) return false;
+    //     if(matrix[current][current]>target) return false;
+    //     int low = current;
+    //     int high = matrix[current].length -1;
+    //     while(low<=high){
+    //         int mid = (low+high)/2;
+    //         if(matrix[current][mid]>target){
+    //             high = mid -1;
+    //         }else if(matrix[current][mid]<target){
+    //             low = mid+1;
+    //         }else{
+    //             return true;
+    //         }
+    //     }
+    //     low = current;
+    //     high = matrix.length - 1;
+    //     while(low<=high){
+    //         int mid = (low+high)/2;
+    //         if(matrix[mid][current]>target){
+    //             high = mid -1;
+    //         }else if(matrix[mid][current]<target){
+    //             low = mid+1;
+    //         }else return true;
+    //     }
+    //     return helper(matrix,current+1,target);
+    // }
     public boolean searchMatrix(int[][] matrix, int target) {
-        // if(matr)
-        return helper(matrix,0,target);
+        int current = 0;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        while(current<row && current<col){
+            if(matrix[current][current]>target) return false;
+            int low = current;
+            int high = col - 1;
+            while(low<=high){
+                int mid = (low+high)/2;
+                if(matrix[current][mid]>target){
+                    high = mid -1;
+                }else if(matrix[current][mid]<target){
+                    low = mid+1;
+                }else{
+                    return true;
+                }
+            }
+            low = current;
+            high = row - 1;
+            while(low<=high){
+                int mid = (low+high)/2;
+                if(matrix[mid][current]>target){
+                    high = mid -1;
+                }else if(matrix[mid][current]<target){
+                    low = mid+1;
+                }else return true;
+            }
+            current++; 
+        }
+        return false;
     }
 }
