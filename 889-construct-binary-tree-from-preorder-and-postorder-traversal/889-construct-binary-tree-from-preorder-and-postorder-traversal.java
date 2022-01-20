7c1066f4930e2382241d7a6a68dc9934cdc7d981
Dev Nirwal <devnirwal16@gmail.com>
2022-01-20 21:19:13 +0530

    Time: 2 ms (43.03%), Space: 40.4 MB (42.46%) - LeetHub

---
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode contructFromPrePost(int[] preorder, int preStart, int preEnd, int[] postorder, int posStart, int posEnd){
+        if(preStart>preEnd) return null;
+        TreeNode root = new TreeNode(preorder[preStart]);
+        if(preStart==preEnd) return root;
+        int index = posEnd;
+        while(index>=posStart){
+            if(postorder[index] == preorder[preStart+1]) break;
+            index--;
+        }
+        int leftTreeSize = index - posStart+1;
+        int rightTreeSize = posEnd - index - 1;
+        root.left = contructFromPrePost(preorder,preStart+1,preStart+leftTreeSize,postorder,posStart,index);
+        root.right = contructFromPrePost(preorder,preEnd- rightTreeSize+1,preEnd,postorder,index+1,posEnd-1);
+        return root;
+    }
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        return contructFromPrePost(preorder,0,preorder.length -1, postorder, 0, postorder.length-1);
+    }
+}
