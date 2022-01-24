0d2e223127ebdefb9e3df3df7954388f435630cc
Dev Nirwal <devnirwal16@gmail.com>
2022-01-24 08:50:42 +0530

    Time: 2 ms (59.30%), Space: 39 MB (34.60%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int n = word.length();
+        if (n == 1) {
+            return true;
+        }
+
+        if (Character.isUpperCase(word.charAt(0)) && Character.isUpperCase(word.charAt(1))) {
+            for (int i = 2; i < n; i++) {
+                if (Character.isLowerCase(word.charAt(i))) {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 1; i < n; i++) {
+                if (Character.isUpperCase(word.charAt(i))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
