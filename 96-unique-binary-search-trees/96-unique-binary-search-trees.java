2b8f1ded98944221ef80442fbfe844dda9cfda2b
Dev Nirwal <devnirwal16@gmail.com>
2022-02-04 12:46:37 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (10.67%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -6,11 +6,9 @@ class Solution {
         dp[1] = 1;
         dp[2] = 2;
         for(int i =3 ;i<n+1;i++){
-            int val = 0;
             for(int j = 0;j<i;j++){
-                val+=(dp[j]*dp[i-j-1]);
+                dp[i]+=(dp[j]*dp[i-j-1]);
             }
-            dp[i] = val;
         }
         return dp[n];
         
