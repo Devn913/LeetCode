7bf5bb4dc59f37bffbfa5de2f351afa5276d8939
Dev Nirwal <devnirwal16@gmail.com>
2022-02-17 22:43:04 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (16.22%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -5,14 +5,14 @@ class Solution {
         int down = 0;
         int right = 0;
         if(row!=m){
-            if(dp[row+1][col]!=0){
+            if(dp[row+1][col]!=-1){
                 down = dp[row+1][col];
             }else{
                 down = uniquePath(m,n,row+1,col,dp,grid);
             }
         }
         if(col!=n){
-            if(dp[row][col+1]!=0){
+            if(dp[row][col+1]!=-1){
                 right = dp[row][col+1];
             }else{
                 right = uniquePath(m,n,row,col+1,dp,grid);
@@ -25,6 +25,9 @@ class Solution {
         int row = obstacleGrid.length;
         int col = obstacleGrid[0].length;
         int[][] dp = new int[row][col];
+        for(int i = 0;i<row;i++){
+            for(int j = 0;j<col;j++) dp[i][j] = -1;
+        }
         return uniquePath(row-1,col-1,0,0,dp,obstacleGrid);
     }
 }
