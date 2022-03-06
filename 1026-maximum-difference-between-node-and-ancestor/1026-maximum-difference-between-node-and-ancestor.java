9dbdc532c6851e1effeee348699253acb60f5f0a
Dev Nirwal <devnirwal16@gmail.com>
2022-03-06 12:38:42 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (52.77%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -14,38 +14,15 @@
  * }
  */
 class Solution {
-    class Node{
-        int max;
-        int min;
-        int maxAns;
-        Node(int max, int min, int maxAns){
-            this.max = max;
-            this.min = min;
-            this.maxAns = maxAns;
-        }
-    }
-    public Node helper(TreeNode root){
-        if(root.left == null && root.right == null) return new Node(root.val,root.val,0);
-        if(root.left == null && root.right!=null){
-            Node right = helper(root.right);
-            int maxAns = Math.max(right.maxAns,Math.max(Math.abs(root.val - right.max),Math.abs(root.val - right.min)));
-            return new Node(Math.max(right.max,root.val),Math.min(right.min,root.val),maxAns);
-        }
-        if(root.left != null && root.right==null){
-            Node right = helper(root.left);
-            int maxAns = Math.max(right.maxAns,Math.max(Math.abs(root.val - right.max),Math.abs(root.val - right.min)));
-            return new Node(Math.max(right.max,root.val),Math.min(right.min,root.val),maxAns);
-        }
-        Node left = helper(root.left);
-        Node right = helper(root.right);
-        int ans1 = Math.max(Math.abs(root.val - left.min),Math.abs(root.val-left.max));
-        int ans2 = Math.max(Math.abs(root.val - right.min),Math.abs(root.val-right.max));
-        int maxAns = Math.max(left.maxAns,Math.max(right.maxAns,Math.max(ans1,ans2)));
-        int max = Math.max(left.max,Math.max(root.val,right.max));
-        int min = Math.min(left.min,Math.min(root.val,right.min));
-        return new Node(max,min,maxAns);
+    public int helper(TreeNode root, int currMax, int currMin){
+        if(root==null) return currMax-currMin;
+        currMax = Math.max(currMax,root.val);
+        currMin = Math.min(currMin,root.val);
+        int left = helper(root.left,currMax,currMin);
+        int right = helper(root.right,currMax,currMin);
+        return Math.max(left,right);
     }
     public int maxAncestorDiff(TreeNode root) {
-        return helper(root).maxAns;
+        return helper(root,root.val,root.val);
     }
 }
