4bf392cb4d25fa22db0a4d7b594e214ef6ffe84c
Dev Nirwal <devnirwal16@gmail.com>
2022-07-17 21:24:55 +0530

    Time: 753 ms (5.65%), Space: 40.9 MB (55.12%) - LeetHub

---
+++ 400-nth-digit/400-nth-digit.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findNthDigit(int n) {
+        if(n<10) return n;
+        if(n==Integer.MAX_VALUE) return 2; 
+        int weight = 1;
+        int counter = 1;
+        long place =10;
+        int num = 1;
+        while(counter+weight <=n){
+            counter+=weight;
+            num++;
+            if(num==place){
+                place = place*10;
+                weight++;
+            }
+        }
+        for(int i = weight-1;i!=-1;i--){
+            if(i==n-counter) return num%10;
+            num = num/10;
+        }
+        return -1;
+    }
+}
