56c62112bc697db55fdcfed92b990c4e42b44d66
Dev Nirwal <devnirwal16@gmail.com>
2023-01-04 15:36:11 +0530

    Time: 90 ms (38.96%), Space: 104.2 MB (40.70%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -1,20 +1,4 @@
 class Solution {
-    public int min(int val, HashMap<Integer,Integer> map){
-        if(map.containsKey(val)) return map.get(val);
-        int temp1 = min(val-2,map);
-        int temp2 = min(val-3,map);
-        if(temp1 == -1 && temp2==-1){
-            map.put(val,-1);
-        }
-        else if(temp1==-1){
-            map.put(val,temp2+1);
-        }else if(temp2==-1){
-            map.put(val,temp1+1);
-        }else{
-            map.put(val,Math.min(temp1,temp2)+1);
-        }
-        return map.get(val);
-    }
     public int minimumRounds(int[] tasks) {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int num: tasks){
