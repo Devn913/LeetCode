4a05a231c01a80bbe9bfb44629aa758fe8d31926
Dev Nirwal <devnirwal16@gmail.com>
2023-01-17 21:20:35 +0530

    Time: 24 ms (27.46%), Space: 42.9 MB (73.56%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -1,12 +1,20 @@
 class Solution {
+    public int countZero(String s){
+        int ans = 0;
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) == '0') ans++;
+        }
+        return ans;
+    }
     public int minFlipsMonoIncr(String s) {
-        int ans = 0, num = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            if (s.charAt(i) == '0') {
-                ans = Math.min(num, ans + 1);
-            } else {
-                ++num;
-            }
+        int zeros = countZero(s);
+        int ones = s.length() - zeros;
+        int ans = Math.min(zeros,ones);
+        int now = 0;
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i) == '1') now++;
+            int curr = now+ (s.length()-i-1)-(ones-now);
+            ans = Math.min(ans,curr);
         }
         return ans;
     }
