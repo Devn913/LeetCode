8530ca38bc5fa48aea7dba95736a19179ff518e9
Dev Nirwal <devnirwal16@gmail.com>
2023-05-05 20:41:01 +0530

    Time: 29 ms (24.19%), Space: 43.1 MB (54.76%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -7,17 +7,15 @@ class Solution {
             }
         }
         int low = 0;
-        int high = k;
         int temp = ans;
-        while(high!=s.length()){
+        while(low+k!=s.length()){
             if(s.charAt(low)=='a' || s.charAt(low) == 'e'|| s.charAt(low) == 'i' || s.charAt(low) == 'o' || s.charAt(low) =='u'){
                 temp--;
             }
-            if(s.charAt(high)=='a' || s.charAt(high) == 'e'|| s.charAt(high) == 'i' || s.charAt(high) == 'o' || s.charAt(high) =='u'){
+            if(s.charAt(low+k)=='a' || s.charAt(low+k) == 'e'|| s.charAt(low+k) == 'i' || s.charAt(low+k) == 'o' || s.charAt(low+k) =='u'){
                 temp++;
             }
             low++;
-            high++;
             ans = Math.max(ans,temp);
         }
         return ans;
