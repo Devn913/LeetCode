b779c104b46093ad42685ed8955a136003e4a408
Dev Nirwal <devnirwal16@gmail.com>
2023-12-01 10:23:49 +0530

    Time: 3 ms (7.87%), Space: 40 MB (90.07%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -5,39 +5,19 @@ class Solution {
         int index2      = 0;
         int subIndex2   = 0;
         while(index1!= word1.length && index2!=word2.length){
+            if(word1[index1].charAt(subIndex1++) != word2[index2].charAt(subIndex2++)){
+                return false;
+            }
             if(subIndex1 == word1[index1].length()){
                 subIndex1 = 0;
                 index1++;
-                continue;
             }
             if(subIndex2 == word2[index2].length()){
                 subIndex2 = 0;
                 index2++;
-                continue;
             }
-            if(word1[index1].charAt(subIndex1) != word2[index2].charAt(subIndex2)){
-                return false;
-            }
-            subIndex1++;
-            subIndex2++;
         }
-        while(index1!=word1.length){
-            if(subIndex1==word1[index1].length()){
-                subIndex1 = 0;
-                index1++;
-                continue;
-            }
-            return false;
-        }
-        while(index2!=word2.length){
-            if(subIndex2==word2[index2].length()){
-                subIndex2= 0 ;
-                index2++;
-                continue;
-            }
-            return false;
-        }
-        return true;
+        return index1==word1.length && index2==word2.length;
         
     }
 }
