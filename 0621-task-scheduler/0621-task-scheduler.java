778bc6320d75a39359139bfffd3aa8d6597dc4f0
Dev Nirwal <devnirwal16@gmail.com>
2024-09-16 00:10:50 +0530

    Time: 23 ms (49.80%), Space: 45.7 MB (46.00%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        // Build frequency map
+        int[] freq = new int[26];
+        for (char ch : tasks) {
+            freq[ch - 'A']++;
+        }
+        
+        // Max heap to store frequencies
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                pq.offer(freq[i]);
+            }
+        }
+
+        int time = 0;
+        // Process tasks until the heap is empty
+        while (!pq.isEmpty()) {
+            int cycle = n + 1;
+            List<Integer> store = new ArrayList<>();
+            int taskCount = 0;
+            // Execute tasks in each cycle
+            while (cycle-- > 0 && !pq.isEmpty()) {
+                int currentFreq = pq.poll();
+                if (currentFreq > 1) {
+                    store.add(currentFreq - 1);
+                }
+                taskCount++;
+            }
+            // Restore updated frequencies to the heap
+            store.forEach(pq::offer);
+            // Add time for the completed cycle
+            time += (pq.isEmpty() ? taskCount : n + 1);
+        }
+        return time;
+    }
+}
