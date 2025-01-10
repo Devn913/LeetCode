e58ed4b2b66a7e1685206e350ab5ff015a700ed3
Dev Nirwal <devnirwal16@gmail.com>
2025-01-11 00:03:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (25.37%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int countZero = 0;
+        int countOne  =  0;
+        for(int num: nums){
+            switch(num){
+                case 0:
+                    countZero++;
+                    break;
+                case 1:
+                    countOne++;
+            }
+        }
+        int currentIndex = 0;
+        while(0<countZero--){
+            nums[currentIndex++] = 0;
+            // countZero--;
+        }
+        while(0<countOne--){
+            nums[currentIndex++] = 1;
+            // countOne--;
+        }
+        while(currentIndex<n){
+            nums[currentIndex++] = 2;
+        }
+    }
+}
