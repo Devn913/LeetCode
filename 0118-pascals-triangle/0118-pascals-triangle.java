689cf99509bd09ed9d89f7ca76ce05b48e051e50
Dev Nirwal <devnirwal16@gmail.com>
2025-01-10 15:28:04 +0530

    Time: 1 ms (85.86%), Space: 42.5 MB (7.12%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,17 +1,13 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
        List<List<Integer>> result = new ArrayList<>();
+       // handling base case
        List<Integer> one = new ArrayList<>();
        one.add(1);
        result.add(one);
        if(numRows==1) return result;
-       List<Integer> two = new ArrayList<>();
-       two.add(1);
-       two.add(1);
-       result.add(two);
-       if(numRows == 2) return result;
-
-       for(int i = 2;i<numRows;i++){
+        
+       for(int i = 1;i<numRows;i++){
             List<Integer> toAdd = new ArrayList<>();
             toAdd.add(1);
             for(int j = 1;j<result.get(i-1).size();j++){
