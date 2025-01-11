97e52de786a30944ae180fcec19ee862c5bd369a
Dev Nirwal <devnirwal16@gmail.com>
2025-01-11 18:50:26 +0530

    Time: 0 ms (100%), Space: 42.1 MB (68.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,29 +1,44 @@
 class Solution {
+    public void swap(int[] arr, int indx1, int indx2){
+        int temp = arr[indx1];
+        arr[indx1] = arr[indx2];
+        arr[indx2] = temp;
+    }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(n==0) return;
         if(m==0){
-            for(int i = 0;i<n;i++){
+            for(int i =0;i<nums2.length;i++){
                 nums1[i] = nums2[i];
             }
             return;
         }
-        int index1 = m-1;
-        while(index1!=-1){
-            nums1[n+index1] = nums1[index1--];
+        if(n==0){
+            return;
         }
-        int current = 0;
-        index1 = n;
-        int index2 = 0;
-        while(index1!=m+n && index2!=n){
-            if(nums1[index1]>nums2[index2]){
-                nums1[current++] = nums2[index2++];
+        int end = nums1.length - 1;
+        int start = m-1;
+        while(start!=-1){
+            swap(nums1,end,start);
+            end--;
+            start--;
+        }
+        int currentIndex = 0;
+        int indx1 = nums1.length-m;
+        int indx2 = 0;
+        while(currentIndex<nums1.length){
+            if(indx2 == nums2.length){
+                nums1[currentIndex++] = nums1[indx1++];
+                continue;
+            }else if(indx1== nums1.length){
+                nums1[currentIndex++] = nums2[indx2++];
+                continue;
+            }
+
+            if(nums1[indx1]>=nums2[indx2]){
+                nums1[currentIndex++] = nums2[indx2++];
             }else{
-                nums1[current++] = nums1[index1++];
+                nums1[currentIndex++] = nums1[indx1++];
             }
         }
-        while(index2!=n ){
-            nums1[current++] = nums2[index2++];
-        }
-        return;
+
     }
 }
