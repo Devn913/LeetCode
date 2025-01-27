ad7e86b4b5b46cb531301c177468f4dd98bad1b7
Dev Nirwal <devnirwal16@gmail.com>
2025-01-27 16:19:56 +0530

    Time: 1 ms (40.1%), Space: 41.5 MB (84.42%) - LeetHub

--- 1983-maximum-population-year/1983-maximum-population-year.java
+++ 1983-maximum-population-year/1983-maximum-population-year.java
@@ -1,21 +1,16 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        int start = Integer.MAX_VALUE;
-        int end   = Integer.MIN_VALUE;
+        int[] population = new int[101];
         for(int[] entry: logs){
-            start = Math.min(start,entry[0]);
-            end   = Math.max(end,entry[1]);
-        } 
-        int[] population = new int[end-start+1];
-        for(int[] entry: logs){
-            for(int i = entry[0]-start;i<=entry[1]-start-1;i++){
-                population[i]++;
+            for(int i = entry[0];i<entry[1];i++){
+                population[i-1950]++;
             }
         }
         int ans = 0;
-        for(int i = 0;i<population.length;i++){
+        for(int i = 0;i<101;i++){
             if(population[ans]<population[i]) ans = i;
         }
-        return start+ans;
+        return ans+1950;
+        
      }
 }
