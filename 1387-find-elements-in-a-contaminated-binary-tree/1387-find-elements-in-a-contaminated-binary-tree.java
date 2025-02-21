edfb751fb737d79d8c9f741d15b647d96fb3e3a2
Dev Nirwal <devnirwal16@gmail.com>
2025-02-22 00:11:31 +0530

    Time: 21 ms (72.24%), Space: 47.4 MB (60.92%) - LeetHub

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class FindElements {
+    HashSet<Integer> set = new HashSet<>();
+    public void helper(TreeNode root, int val){
+        if(root == null) return;
+        set.add(val);
+        helper(root.left,2*val+1);
+        helper(root.right,2*val+2);
+        
+    }
+
+    public FindElements(TreeNode root) {
+        helper(root,0);
+    }
+    
+    public boolean find(int target) {
+        return set.contains(target);
+    }
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements obj = new FindElements(root);
+ * boolean param_1 = obj.find(target);
+ */
