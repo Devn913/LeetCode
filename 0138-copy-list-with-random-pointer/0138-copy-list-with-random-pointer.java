6905ddaf390b019423a12f641a89b31efc671df5
Dev Nirwal <devnirwal16@gmail.com>
2025-03-01 23:17:21 +0530

    Time: 0 ms (100%), Space: 44.4 MB (82.29%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,19 +15,52 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head == null) return head;
-        HashMap<Node,Node> map = new HashMap<>();
-        Node p = head;
-        while(p!=null){
-            map.put(p,new Node(p.val));
-            p = p.next;
+        if(head == null ) return head;
+        if(head.next == null){
+            if(head.random == null){
+                return new Node(head.val);
+            }else{
+                Node head2 = new Node(head.val);
+                head2.random = head2;
+                return head2;
+            }
         }
-        p = head;
-        while(p!=null){
-            map.get(p).next = map.get(p.next);
-            map.get(p).random = map.get(p.random);
-            p = p.next;
+        Node curr = head;
+        Node next = curr;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = new Node(curr.val);
+            curr.next.next = next;
+            curr  = next;
         }
-        return map.get(head);
+        curr = head;
+        while(curr!=null){
+            next = curr.next;
+            if(curr.random ==null){
+                next.random = null;
+                curr = next.next;
+                continue;
+            }
+            next.random = curr.random.next;
+            curr = next.next;
+        }
+        Node prev = head;
+        curr = head.next;
+        Node res = curr;
+        while(prev!=null){
+            curr = prev.next;
+            next = curr.next;
+            prev.next = curr.next;
+            if(next==null){
+                curr.next = null;
+            }else{
+                 curr.next = next.next;
+            }
+            prev = prev.next;
+            
+        }
+        return res;
+        
     }
+
 }
