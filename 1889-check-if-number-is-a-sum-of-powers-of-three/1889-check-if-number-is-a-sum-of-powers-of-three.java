c2c351711b428e3e03744d48df7f0533c87d629b
Dev Nirwal <devnirwal16@gmail.com>
2025-03-05 00:24:50 +0530

    Time: 218 ms (6.98%), Space: 55.2 MB (9.3%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,5 +1,9 @@
 class Solution {
-    HashSet<Integer> set = new HashSet<>();
+
+    // Approach 1: Pre-computation all possible sum of power of three
+    
+    HashSet<Integer> set = new HashSet<>(); 
+    // function to generate sum of all subset of an array.
     public void generateAllSubsetSum(ArrayList<Integer> list, int index, int sum){
         if(index == list.size()){
             set.add(sum);
@@ -11,12 +15,16 @@ class Solution {
 
     }
     public boolean checkPowersOfThree(int n) {
+        // array to save all powers of three.
         ArrayList<Integer> powerOfThree = new ArrayList<>();
+        
         int currentNum = 1;
+        
         while(currentNum<=10000000){
             powerOfThree.add(currentNum);
             currentNum*=3;
         }
+        // generate all subset sum of powers of three.
         generateAllSubsetSum(powerOfThree,0,0);
         return set.contains(n);
         
