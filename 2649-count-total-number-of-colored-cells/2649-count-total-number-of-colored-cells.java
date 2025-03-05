e6be2d160190617e3be802d818e3c0164c87b7ec
Dev Nirwal <devnirwal16@gmail.com>
2025-03-05 21:25:18 +0530

    Time: 0 ms (100%), Space: 40.4 MB (73.36%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.java
@@ -1,12 +1,19 @@
 class Solution {
     public long coloredCells(int n) {
-        // 1, 1+4, 1+4+8, 1+4+8+12 ...
-        long ans = 1;
-        long last = 4;
-        while(n--!=1){
-            ans +=  last;
-            last +=4;
-        } 
-        return ans;
+
+        // Approach 2: 1 + (4×1) + (4×2) +...+ (4×(n−1)) = 1 + 2 × (n−1) × n
+        return 1 + (long) n * (n - 1) * 2;
+
+
+
+
+        // // Approach 1:  1, 1+4, 1+4+8, 1+4+8+12 ...
+        // long ans = 1;
+        // long last = 4;
+        // while(n--!=1){
+        //     ans +=  last;
+        //     last +=4;
+        // } 
+        // return ans;
     }
 }
