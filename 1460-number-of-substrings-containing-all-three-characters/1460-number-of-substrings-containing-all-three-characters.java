e08ed95ddd9e1075fcd229a35b34e0aa13e3916d
Dev Nirwal <devnirwal16@gmail.com>
2025-03-11 11:19:56 +0530

    Time: 18 ms (25.14%), Space: 45 MB (29.09%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -5,7 +5,7 @@ class Solution {
         int end = 0;
         int ans = 0;
         freq[s.charAt(end) - 'a']++;
-        while(start<s.length() && end<s.length()){
+        while(end<s.length()){
             if(freq[0] !=0 && freq[1] != 0 && freq[2] !=0){
                 ans+=(s.length() - end);
                 freq[s.charAt(start)-'a']--;
@@ -13,10 +13,10 @@ class Solution {
             }else if(end<s.length()-1){
                 end++;
                 freq[s.charAt(end)-'a']++;
-            }else{
+            }else if(start<s.length()){
                 freq[s.charAt(start)-'a']--;
                 start++;
-            }
+            }else break;
 
             
 
