4a7b28a98e61af44aa7271d355e375ed0c0ddccf
Dev Nirwal <devnirwal16@gmail.com>
2025-03-13 00:02:53 +0530

    Time: 0 ms (100%), Space: 42.7 MB (31.5%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,20 +1,15 @@
 class Solution {
-    public double helper(double x,long n){
-        if(n==0) return 1;
-        if(n%2==0){
-            return helper(x*x,n/2);
-        }else{
-            return x*helper(x,--n);
-
-        }
-
-    }
     public double myPow(double x, int n) {
-        if(n<0){
-            long nn = n;
-            nn = -1*nn;
-            return (1.0)/helper(x,nn);
+        long nn = n;   
+        if(n<0) { nn = -1*nn; x = 1/x;}
+        double tempAns = x;
+        double ans = 1;
+        while(nn!=0){
+            int bit =(int) (nn & 1);
+            if(bit==1) ans = ans*tempAns;
+            tempAns = tempAns*tempAns;
+            nn = nn >> 1;
         }
-        return helper(x,n);
+        return ans;
     }
 }
