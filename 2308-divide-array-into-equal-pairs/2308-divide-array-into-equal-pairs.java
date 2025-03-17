290184d5c9bd3cb2b2357544eab32a751d51ecb8
Dev Nirwal <devnirwal16@gmail.com>
2025-03-17 12:28:02 +0530

    Time: 2 ms (82.79%), Space: 44.2 MB (85.16%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,9 +1,28 @@
+// class Solution {
+// Approach 1: Using Sorting
+//     public boolean divideArray(int[] arr) {
+//       Arrays.sort(arr);
+//       for(int i = 0;i<arr.length;i+=2){
+//         if(arr[i]!=arr[i+1]) return false;
+//       }  
+//       return true;
+//     }
+// }
+
 class Solution {
     public boolean divideArray(int[] arr) {
-      Arrays.sort(arr);
-      for(int i = 0;i<arr.length;i+=2){
-        if(arr[i]!=arr[i+1]) return false;
-      }  
-      return true;
+    // Approach 2: using Frequency array.
+        int max = 0;
+        for(int num: arr){
+            max = Math.max(max,num);
+        }
+        int[] freq = new int[max+1];
+        for(int num: arr){
+            freq[num]++;
+        }
+        for(int i = 0;i<=max;i++){
+            if(freq[i]%2!=0) return false;
+        }
+        return true;
     }
 }
