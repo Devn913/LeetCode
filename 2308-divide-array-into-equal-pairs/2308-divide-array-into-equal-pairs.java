7b16ae936110eb97f9dedaaf75011210007f341b
Dev Nirwal <devnirwal16@gmail.com>
2025-03-17 12:30:18 +0530

    Time: 1 ms (100%), Space: 44.3 MB (85.16%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,5 +1,6 @@
 // class Solution {
 // Approach 1: Using Sorting
+// Time: O(n*log(n)), Space: O(1)
 //     public boolean divideArray(int[] arr) {
 //       Arrays.sort(arr);
 //       for(int i = 0;i<arr.length;i+=2){
@@ -9,18 +10,34 @@
 //     }
 // }
 
+// class Solution {
+//     public boolean divideArray(int[] arr) {
+//     // Approach 2: using Frequency array.
+//     // Time: O(n), Space: O(n)
+//         int max = 0;
+//         for(int num: arr){
+//             max = Math.max(max,num);
+//         }
+//         int[] freq = new int[max+1];
+//         for(int num: arr){
+//             freq[num]++;
+//         }
+//         for(int i = 0;i<=max;i++){
+//             if(freq[i]%2!=0) return false;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean divideArray(int[] arr) {
-    // Approach 2: using Frequency array.
-        int max = 0;
-        for(int num: arr){
-            max = Math.max(max,num);
-        }
-        int[] freq = new int[max+1];
+    // Approach 3: using Frequency array.
+    // Time: O(n), Space: O(1)
+        int[] freq = new int[501];
         for(int num: arr){
             freq[num]++;
         }
-        for(int i = 0;i<=max;i++){
+        for(int i = 0;i<=500;i++){
             if(freq[i]%2!=0) return false;
         }
         return true;
