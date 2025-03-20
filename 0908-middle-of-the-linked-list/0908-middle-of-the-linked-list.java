2e103aa8a958a3b11b39e978b0f3a0a214a1eb84
Dev Nirwal <devnirwal16@gmail.com>
2025-03-20 09:41:14 +0530

    Time: 0 ms (100%), Space: 41.2 MB (56.48%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -11,19 +11,48 @@
 class Solution {
     public ListNode middleNode(ListNode head) {
         // Approach 1: Count  (Iteratively)
+        // if(head == null) return null;
+        // ListNode temp = head;
+        // int count = 0;
+        // while(temp!=null){
+        //     count++;
+        //     temp = temp.next;
+        // }
+        // int middleIndex = (count)/2;
+        // temp = head;
+        // while(middleIndex !=0){
+        //     temp = temp.next;
+        //     middleIndex--;
+        // }
+        // return temp;
+
+        // Approach 2: Slow and Fast pointer approach
         if(head == null) return null;
-        ListNode temp = head;
-        int count = 0;
-        while(temp!=null){
-            count++;
-            temp = temp.next;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int middleIndex = (count)/2;
-        temp = head;
-        while(middleIndex !=0){
-            temp = temp.next;
-            middleIndex--;
-        }
-        return temp;
+        return slow;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
