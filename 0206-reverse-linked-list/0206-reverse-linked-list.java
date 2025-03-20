4caa9f58c48e159338e451105ff55f40b3448285
Dev Nirwal <devnirwal16@gmail.com>
2025-03-20 11:13:46 +0530

    Time: 0 ms (100%), Space: 42.8 MB (28.31%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,22 +10,41 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        // Approach 1: Cope Linked list to array.
-        // Initialize a list
-        ArrayList<Integer> list = new ArrayList<>();
-        // Traversal
-        ListNode temp = head;
-        while(temp!=null){
-            list.add(temp.val);
-            temp = temp.next;
-        }
-        temp = head;
-        for(int i = list.size()-1;i!=-1;i--){
-            System.out.println(list.get(i));
-            temp.val = list.get(i);
-            temp = temp.next;
-        }
-        return head;
+        // // Approach 1: Copy Linked list to array.
+        // // TC : O(N)
+        // // SC : O(N)
+        // // Initialize a list
+        // ArrayList<Integer> list = new ArrayList<>();
+        // // Traversal
+        // ListNode temp = head;
+        // while(temp!=null){
+        //     list.add(temp.val);
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // for(int i = list.size()-1;i!=-1;i--){
+        //     System.out.println(list.get(i));
+        //     temp.val = list.get(i);
+        //     temp = temp.next;
+        // }
+        // return head;
+
+
+        // Approach 2: Optimial
+
+        if(head == null || head.next == null) return head;
+
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = head.next;
+        while(next!=null){
+            curr.next = prev;
+            prev  =curr;
+            curr = next;
+            next = next.next;
+        } 
+        curr.next = prev;
+        return curr;
 
     }
 }
