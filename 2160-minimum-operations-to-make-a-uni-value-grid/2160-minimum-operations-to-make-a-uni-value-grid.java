ad586f6fa6e60077283d44acca1236dd133e82b3
Dev Nirwal <devnirwal16@gmail.com>
2025-03-30 19:44:50 +0530

    Time: 37 ms (78.96%), Space: 78.3 MB (8.86%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,52 +1,25 @@
 class Solution {
 
     public int minOperations(int[][] grid, int x) {
-        List<Integer> numsArray = new ArrayList<>();
-        int result = 0;
-
-        // Flatten the grid into numsArray and check remainder condition
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                if (grid[row][col] % x != grid[0][0] % x) return -1;
-                // If any element has a different remainder than the first
-                // element, it is impossible to make all elements equal, so
-                // return -1
-                numsArray.add(grid[row][col]);
+        int[] ansArray = new int[grid.length * grid[0].length];
+        
+        int index = 0;
+        for(int i = 0;i<grid.length;i++){
+            for(int j = 0;j<grid[i].length;j++){
+                ansArray[index] = grid[i][j];
+                index++;
             }
         }
-
-        Collections.sort(numsArray);
-
-        int length = numsArray.size();
-        int prefixIndex = 0;
-        int suffixIndex = length - 1;
-
-        // Move prefixIndex and suffixIndex towards the middle
-        while (prefixIndex < suffixIndex) {
-            // If the prefix of equal elements is shorter than the suffix
-            if (prefixIndex < length - suffixIndex - 1) {
-                // Calculate the number of operations required to extend the prefix
-                int prefixOperations =
-                    ((prefixIndex + 1) *
-                        (numsArray.get(prefixIndex + 1) -
-                            numsArray.get(prefixIndex))) /
-                    x;
-                result += prefixOperations;
-                // Move the prefix index forward
-                prefixIndex++;
-            } else {
-                // Calculate the number of operations required to extend the suffix
-                int suffixOperations =
-                    ((length - suffixIndex) *
-                        (numsArray.get(suffixIndex) -
-                            numsArray.get(suffixIndex - 1))) /
-                    x;
-                result += suffixOperations;
-                // Move the suffix index backward
-                suffixIndex--;
-            }
+        Arrays.sort(ansArray);
+        int median = ansArray[ansArray.length / 2];
+        int ans = 0;
+        int remiander = grid[0][0] % x;
+        for (int num : ansArray) {
+            if (num % x != remiander)
+                return -1;
+            ans += (Math.abs(median - num) / x);
         }
+        return ans;
 
-        return result;
     }
 }
