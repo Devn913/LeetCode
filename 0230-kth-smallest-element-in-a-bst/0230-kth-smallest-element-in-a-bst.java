48b4f94dbb14479cd397a59202363d34e1cc6642
Dev Nirwal <devnirwal16@gmail.com>
2025-04-07 10:21:51 +0530

    Time: 0 ms (100%), Space: 44.4 MB (60.68%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,20 +14,41 @@
  * }
  */
 class Solution {
+
     ArrayList<Integer> result = new ArrayList<>();
 
-    public void inorder(TreeNode root){
+    public void helper(TreeNode root, int k){
         if(root == null) return;
-        inorder(root.left);
-        result.add(root.val);
-        inorder(root.right);
-    }
-    public int kthSmallest(TreeNode root, int k) {
-        inorder(root);
+        if(result.size() == k) return;
 
+        helper(root.left,k);
+        if(result.size() == k) return;
+        result.add(root.val);
+        if(result.size() == k) return;
+        helper(root.right,k);
+        
+    } 
+
+    public int kthSmallest(TreeNode root, int k) {
+        helper(root,k);
         return result.get(k-1);
+    }
+
+
+    // Approach: 1 Brute Forc
+    // ArrayList<Integer> result = new ArrayList<>();
+
+    // public void inorder(TreeNode root){
+    //     if(root == null) return;
+    //     inorder(root.left);
+    //     result.add(root.val);
+    //     inorder(root.right);
+    // }
+
+    // public int kthSmallest(TreeNode root, int k) {
+    //     inorder(root);
+    //     return result.get(k-1);
         
 
 
-    }
 }
