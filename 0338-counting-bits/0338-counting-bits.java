bbf2edc3638479a31fdc8e994087193d9c7fa3f6
Dev Nirwal <devnirwal16@gmail.com>
2025-04-07 14:57:38 +0530

    Time: 2 ms (94.91%), Space: 47.2 MB (7.62%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int[] ans = new int[n+1];
         ans[0] = 0;
         for(int i = 1;i<=n;i++){
-            ans[i] = ans[i/2]+(i%2);
+            ans[i] = ans[i>>1]+(i%2);
         }
         return ans;
     }
