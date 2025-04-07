a99415e5f33eb43638702cf18d3dccf2ac74c457
Dev Nirwal <devnirwal16@gmail.com>
2025-04-07 10:46:32 +0530

    Time: 0 ms (100%), Space: 44.8 MB (15.77%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,25 +14,45 @@
  * }
  */
 class Solution {
+    int count = 0;
 
-    ArrayList<Integer> result = new ArrayList<>();
+    public int helper(TreeNode root, int k){
+        if(root == null) return -1;
+        if(count == k) return root.val;
+        int left = helper(root.left,k);
+        if(count == k) return left;
+        count++;
+        if(count == k) return root.val;
+        return helper(root.right,k);
 
-    public void helper(TreeNode root, int k){
-        if(root == null) return;
-        if(result.size() == k) return;
 
-        helper(root.left,k);
-        if(result.size() == k) return;
-        result.add(root.val);
-        if(result.size() == k) return;
-        helper(root.right,k);
-        
-    } 
-
-    public int kthSmallest(TreeNode root, int k) {
-        helper(root,k);
-        return result.get(k-1);
     }
+    
+    public int kthSmallest(TreeNode root, int k) {
+        return helper(root,k);
+     }
+
+
+
+    // Approach 2: Save only k elements
+    // ArrayList<Integer> result = new ArrayList<>();
+
+    // public void helper(TreeNode root, int k){
+    //     if(root == null) return;
+    //     if(result.size() == k) return;
+
+    //     helper(root.left,k);
+    //     if(result.size() == k) return;
+    //     result.add(root.val);
+    //     if(result.size() == k) return;
+    //     helper(root.right,k);
+        
+    // } 
+
+    // public int kthSmallest(TreeNode root, int k) {
+    //     helper(root,k);
+    //     return result.get(k-1);
+    // }
 
 
     // Approach: 1 Brute Forc
