df9217f38f31d90347b9514e1463cb2415220c2c
Dev Nirwal <devnirwal16@gmail.com>
2025-04-11 09:50:43 +0530

    Time: 5 ms (37.27%), Space: 44.7 MB (28.34%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -10,16 +10,14 @@
  */
 class Solution {
     public class Pair implements Comparable<Pair>{
-        int val;
         ListNode head;
 
-        Pair(int val, ListNode head){
-            this.val = val;
+        Pair(ListNode head){
             this.head = head;
         }
         @Override
         public int compareTo(Pair p){
-            return this.val - p.val;
+            return this.head.val - p.head.val;
         }
     }
     public ListNode mergeKLists(ListNode[] lists) {
@@ -28,14 +26,14 @@ class Solution {
 
         for(int i = 0;i<lists.length;i++){
             ListNode head = lists[i];
-            if(head!=null) pq.add(new Pair(head.val,head));
+            if(head!=null) pq.add(new Pair(head));
         }
 
         ListNode head = new ListNode(0);
         ListNode tail = head;
         while(pq.size()!=0){
             ListNode temp = pq.remove().head;
-            if(temp.next!=null) pq.add(new Pair(temp.next.val,temp.next));
+            if(temp.next!=null) pq.add(new Pair(temp.next));
             tail.next = temp;
             tail = tail.next;
         }
