8dd2186506377aae66615c2ba729d624f7db3e8e
Dev Nirwal <devnirwal16@gmail.com>
2025-04-16 19:36:55 +0530

    Time: 5 ms (74.81%), Space: 58 MB (54.74%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,14 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
-        int low = 0;
+
+        // Initilization the two pointer 
+        int low  = 0;
         int high = height.length -1;
 
         int ans = Integer.MIN_VALUE;
         while(low<high){
-            int currHeight = Math.min(height[low],height[high]);
+            // finding the min height for the water level
+            int currHeight = Math.min(height[low],height[high]); 
+
+            // finding the width
             int width  = high-low;
+
+            // calculating the area and updating the maximum area
             ans = Math.max(ans,currHeight*width);
 
+            // Changing the pointer which has minimum height
             if(height[low]>height[high]){
                 high--;
             } else{
