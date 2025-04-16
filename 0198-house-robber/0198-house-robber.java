7f1a7113da69c7216efc71965bb05cc4174d3b17
Dev Nirwal <devnirwal16@gmail.com>
2025-04-16 20:00:02 +0530

    Time: 0 ms (100%), Space: 40.5 MB (99.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -12,24 +12,45 @@
 // }
 
 
-// Method 2: Memoization
-class Solution {
-    public int helper(int[] nums, int[] dp, int currentIndex){
+// Method 2: Memoization: Accepted 
+// Time complexity:  O(n)
+// Space Complexity: O(n) + recursion stack memory
+// class Solution {
+//     public int helper(int[] nums, int[] dp, int currentIndex){
+//         if(currentIndex >= nums.length) return 0;
+//         if(dp[currentIndex] != -1) return dp[currentIndex];
+//         int currentHouse = nums[currentIndex] + helper(nums,dp,currentIndex+2);
+//         dp[currentIndex] = Math.max(currentHouse, helper(nums,dp,currentIndex+1));
+//         return dp[currentIndex];
+//     }
+//     public int rob(int[] nums) {
+//         int[] dp = new int[nums.length];
+//         for(int i =0;i<nums.length;i++){
+//             dp[i] = -1;
+//         }
+//         return helper(nums,dp,0);
+//     }
+// }
 
-        if(currentIndex >= nums.length) return 0;
-        if(dp[currentIndex] != -1) return dp[currentIndex];
-        int currentHouse = nums[currentIndex] + helper(nums,dp,currentIndex+2);
-        dp[currentIndex] = Math.max(currentHouse, helper(nums,dp,currentIndex+1));
-        return dp[currentIndex];
-    }
+
+class Solution {
     public int rob(int[] nums) {
+        if(nums.length  == 1) return nums[0];
+        if(nums.length  == 2) return Math.max(nums[0],nums[1]);
         int[] dp = new int[nums.length];
         for(int i =0;i<nums.length;i++){
             dp[i] = -1;
         }
-        return helper(nums,dp,0);
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0],nums[1]);
+
+        for(int i = 2;i<nums.length;i++){
+            dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.length - 1];
     }
 }
+
 
 
 
