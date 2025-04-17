77f31f968e462764b0a5ef5bacbd0769a2f144dd
Dev Nirwal <devnirwal16@gmail.com>
2025-04-17 22:35:22 +0530

    Time: 0 ms (100%), Space: 40.2 MB (88.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,3 @@
-class Solution {
-    public int helper(int m, int n, int i, int j, int[][] dp){
-        if(i>= m || j>=n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i ==  m-1 && j == n-1) return 1;
-        dp[i][j] =  helper(m,n,i+1,j,dp) + helper(m,n,i,j+1,dp);
-        return dp[i][j];
-        
-    }
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                dp[i][j] = -1;
-            }
-        }
-        return helper(m,n,0,0,dp);
-    }
-}
-
-
 // Recursion: TLE
 // class Solution {
 //     public int helper(int m, int n, int i, int j){
@@ -30,3 +9,46 @@ class Solution {
 //         return helper(m,n,0,0);
 //     }
 // }
+
+// Method: 2 Memoization
+// class Solution {
+//     public int helper(int m, int n, int i, int j, int[][] dp){
+//         if(i>= m || j>=n) return 0;
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         if(i ==  m-1 && j == n-1) return 1;
+//         dp[i][j] =  helper(m,n,i+1,j,dp) + helper(m,n,i,j+1,dp);
+//         return dp[i][j];
+//     }
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         for(int i = 0;i<m;i++){
+//             for(int j = 0;j<n;j++){
+//                 dp[i][j] = -1;
+//             }
+//         }
+//         return helper(m,n,0,0,dp);
+//     }
+// }
+
+// Method: 3 Dynammic Programming
+// Time Complexity  : O(m*n)
+// Space Complexity : O(m*n) 
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0;i<n;i++){
+            dp[m-1][i] = 1;
+        }
+        for(int i = 0;i<m;i++){
+            dp[i][n-1] = 1;
+        }
+        for(int i = m-2;i>=0;i--){
+            for(int j = n-2;j>=0;j--){
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+}
+
+
