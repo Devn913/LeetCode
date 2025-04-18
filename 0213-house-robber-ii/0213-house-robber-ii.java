ca01c5e85e3bc4ebb34073a812c93be6b3d3c43b
Dev Nirwal <devnirwal16@gmail.com>
2025-04-18 15:11:26 +0530

    Time: 0 ms (100%), Space: 41.1 MB (64.75%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,29 @@
 class Solution {
+    public int helper(int[] nums, int[] memo, int index,  int exclude){
+        if(index== exclude) return 0;
+        if(index>=nums.length) return 0;
+        if(memo[index]!=-1) return memo[index];
+
+        int currentHouseLoot = nums[index]+ helper(nums, memo, index+2,exclude);
+        int notLoot = helper(nums,memo,index+1,exclude);
+        memo[index] = Math.max(currentHouseLoot,notLoot);
+        return memo[index];
+
+    }
 
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        if(nums.length == 2) return Math.max(nums[0],nums[1]);
-        if(nums.length == 3) return Math.max(nums[0],Math.max(nums[1],nums[2]));
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        int ans1 = 0;
-        for(int i = 2;i<nums.length - 1;i++){
-            dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
-            ans1 = dp[i];
-            System.out.println(Arrays.toString(dp));
+        if(nums.length  ==1) return nums[0];
+
+        int[] memo = new int[nums.length];
+        for(int i = 0;i<nums.length;i++){
+            memo[i] = -1;
         }
-        int ans2 = 0;
-        dp[1] = nums[1];
-        dp[2] = Math.max(nums[1], nums[2]);
-        for(int i = 3;i<nums.length;i++){
-            dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i]);
-            ans2 = dp[i];
-            System.out.println(Arrays.toString(dp));
+
+        int ans1 = helper(nums,memo,0,nums.length -1);
+        for(int i = 0;i<nums.length;i++){
+            memo[i] = -1;
         }
+        int ans2 = helper(nums,memo,1,0);
         return Math.max(ans1,ans2);
     
     }
