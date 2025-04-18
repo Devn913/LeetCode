bf4807212810290a7a1d569b030a0a91f670982c
Dev Nirwal <devnirwal16@gmail.com>
2025-04-18 09:28:43 +0530

    Time: 0 ms (100%), Space: 40.7 MB (18.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,36 @@
 class Solution {
-    public int helper(int[] memo, int n){
-        if(n == 1) return 1;
-        if(n==2) return 2;
-        if(memo[n]!=-1) return memo[n];
-        memo[n] = helper(memo,n-1)+helper(memo,n-2);
-        return memo[n];
-         
-    }
+    // Approach 3: Dynammic Programming
     public int climbStairs(int n) {
-        int[] memo = new int[n+1];
-        for(int i = 0;i<=n;i++){
-            memo[i] = -1;
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for(int i = 2;i<n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
-        return helper(memo,n);
+        return dp[n-1];
     }
+
+
+
+    // Approach 2: Memoization with HashMap
+    // HashMap<Integer,Integer> map = new HashMap<>();
+    // public int climbStairs(int n) {
+    //     if(n == 1) return 1;
+    //     if(n == 2) return 2;
+    //     if(map.containsKey(n)) return map.get(n);
+    //     int nAns = climbStairs(n-1) + climbStairs(n-2);
+    //     map.put(n,nAns);
+    //     return nAns;
+    // }
+    
+    // Approach 1: Recursion (TLE)
+    // public int climbStairs(int n) {
+    //     if(n == 1) return 1;
+    //     if(n == 2) return 2;
+    //     return climbStairs(n-1) + climbStairs(n-2);
+
+    // }
 }
