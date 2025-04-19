8dd6b1ab45deea1c4408fec9f33cf893c6c9126c
Dev Nirwal <devnirwal16@gmail.com>
2025-04-20 04:06:04 +0530

    Time: 4 ms (62.14%), Space: 47.6 MB (58.1%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -7,24 +7,14 @@ class Solution {
         for(int i = 0;i<grid.length-1;i++){
             dp[i][grid[0].length] = 500000;
         }
-        for(int i = 0;i<dp.length;i++){
-            for(int j = 0;j<dp[0].length;j++){
-                System.out.print(dp[i][j]+ " ");
-            }
-            System.out.println();
-        }
+
         for(int i = grid.length-1;i!=-1;i--){
             for(int j = grid[0].length-1;j!=-1;j--){
                 dp[i][j] = grid[i][j] + Math.min(dp[i+1][j],dp[i][j+1]);
             }
         }
 
-        // for(int i = 0;i<dp.length;i++){
-        //     for(int j = 0;j<dp[0].length;j++){
-        //         System.out.print(dp[i][j]+ " ");
-        //     }
-        //     System.out.println();
-        // }
+
         return dp[0][0];
     }
 }
