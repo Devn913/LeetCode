3c57a9ec84eab629f91f149e3e1ec8cc07e76f00
Dev Nirwal <devnirwal16@gmail.com>
2025-04-19 23:22:42 +0530

    Time: 31 ms (13.66%), Space: 51.1 MB (30.82%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,13 +1,13 @@
 class Solution {
-    public int LCS(int[][] memo, String s1, String s2, int i, int j, int len1, int len2){
-        if(i>=len1 || j>=len2) return 0;
-        if(memo[i][j] != -1) return memo[i][j];
-        if(s1.charAt(i) == s2.charAt(j)){
-            memo[i][j] = 1 + LCS(memo, s1, s2, i+1, j+1, len1,len2);
+    public int LCS(int[][] memo, String s1, String s2, int m, int n){
+        if(m<0 || n<0) return 0;
+        if(memo[m][n] != -1) return memo[m][n];
+        if(s1.charAt(m) == s2.charAt(n)){
+            memo[m][n] = 1 + LCS(memo, s1, s2, m-1,n-1);
         }else{
-            memo[i][j] = Math.max(LCS(memo,s1,s2,i+1,j,len1,len2),LCS(memo,s1,s2,i,j+1,len1,len2));
+            memo[m][n] = Math.max(LCS(memo,s1,s2,m-1,n),LCS(memo,s1,s2,m,n-1));
         }
-        return memo[i][j];
+        return memo[m][n];
     }
     
     public int longestCommonSubsequence(String str1, String str2) {
@@ -20,6 +20,6 @@ class Solution {
                 memo[i][j] = -1;
             }
         }
-        return LCS(memo,str1,str2,0,0,m,n);
+        return LCS(memo,str1,str2,m-1,n-1);
     }
 }
