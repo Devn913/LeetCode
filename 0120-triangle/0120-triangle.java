8b164e8f9592bc4330e656c2b54cc5a9e0d4db3c
Dev Nirwal <devnirwal16@gmail.com>
2025-04-20 03:51:17 +0530

    Time: 3 ms (64.58%), Space: 44.4 MB (37.18%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,4 +1,4 @@
-// Approach: 1 TLE
+// Approach: 1 Recursion: TLE
 // class Solution {
 //     public int helper(List<List<Integer>> traingle, int currentRow, int currentCol){
 //         if(currentRow>=traingle.size() || currentCol>=traingle.get(currentRow).size()) return 0;
@@ -12,25 +12,43 @@
 //     }
 // }
 
-class Solution {
-    public int helper(List<List<Integer>> traingle,int[][] memo, int currentRow, int currentCol){
-        if(currentRow>=traingle.size() || currentCol>=traingle.get(currentRow).size()) return 0;
+// Approach 2: Memoization
+// class Solution {
+//     public int helper(List<List<Integer>> traingle,int[][] memo, int currentRow, int currentCol){
+//         if(currentRow>=traingle.size() || currentCol>=traingle.get(currentRow).size()) return 0;
+//         if(memo[currentRow][currentCol]!=-1) return memo[currentRow][currentCol];
+//         memo[currentRow][currentCol]  = traingle.get(currentRow).get(currentCol) + Math.min(
+//                         helper(traingle,memo,currentRow+1,currentCol),
+//                         helper(traingle,memo,currentRow+1,currentCol+1)
+//                     );
+//         return memo[currentRow][currentCol];
+//     }
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int[][] memo = new int[triangle.size()][triangle.size()];
+//         for(int i = 0;i<memo.length;i++){
+//             for(int j = 0;j<memo.length;j++){
+//                 memo[i][j] = -1;
+//             }
+//         }
+//         return helper(triangle,memo,0,0);
+//     }
+// }
 
-        if(memo[currentRow][currentCol]!=-1) return memo[currentRow][currentCol];
-        memo[currentRow][currentCol]  = traingle.get(currentRow).get(currentCol) + Math.min(
-                        helper(traingle,memo,currentRow+1,currentCol),
-                        helper(traingle,memo,currentRow+1,currentCol+1)
-                    );
-        return memo[currentRow][currentCol];
-    }
+// Approach 3: Dyannamic Programming
+class Solution {
+
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] memo = new int[triangle.size()][triangle.size()];
-        for(int i = 0;i<memo.length;i++){
-            for(int j = 0;j<memo.length;j++){
-                memo[i][j] = -1;
+        int[][] dp = new int[triangle.size()+1][triangle.size()+1];
+
+        for(int i = triangle.size() - 1;i!=-1;i--){
+            for(int j = triangle.get(i).size()-1;j!=-1;j--){
+                dp[i][j]  = triangle.get(i).get(j) + Math.min(
+                        dp[i+1][j],
+                        dp[i+1][j+1]
+                );
             }
         }
 
-        return helper(triangle,memo,0,0);
+        return dp[0][0];
     }
 }
