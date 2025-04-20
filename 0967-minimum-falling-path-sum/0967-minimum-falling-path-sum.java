7033de9547ef552cca266d10afe3cc1298afae27
Dev Nirwal <devnirwal16@gmail.com>
2025-04-20 15:52:42 +0530

    Time: 9 ms (20.12%), Space: 44.8 MB (97.01%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.java
@@ -1,23 +1,49 @@
+// Approach: 1 Dynnamic Programming
+// class Solution {
+//     public int minFallingPathSum(int[][] matrix) {
+//         int[][] dp = new int[matrix.length][matrix.length];
+//         int ans = Integer.MAX_VALUE;
+//         for(int i =0;i<matrix.length;i++){
+//             dp[0][i] = matrix[0][i];
+//             ans = Math.min(ans,dp[0][i]);
+//         }
+//         for (int i = 1; i < matrix.length; i++) {
+//             for(int j = 0;j<matrix.length;j++){
+//                 if(j == 0){
+//                     dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j],dp[i-1][j+1]);
+//                     ans = dp[i][j]; 
+//                 }else if(j == matrix.length -1){
+//                     dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j],dp[i-1][j-1]);
+//                     ans = Math.min(ans,dp[i][j]);
+//                 }else{
+//                     dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i-1][j+1]));
+//                     ans = Math.min(ans,dp[i][j]);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+// Approach 2: 
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int[][] dp = new int[matrix.length][matrix.length];
         int ans = Integer.MAX_VALUE;
         for(int i =0;i<matrix.length;i++){
-            dp[0][i] = matrix[0][i];
-            ans = Math.min(ans,dp[0][i]);
+            ans = Math.min(ans,matrix[0][i]);
         }
 
         for (int i = 1; i < matrix.length; i++) {
             for(int j = 0;j<matrix.length;j++){
                 if(j == 0){
-                    dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j],dp[i-1][j+1]);
-                    ans = dp[i][j]; 
+                    matrix[i][j] = matrix[i][j] + Math.min(matrix[i-1][j],matrix[i-1][j+1]);
+                    ans = matrix[i][j]; 
                 }else if(j == matrix.length -1){
-                    dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j],dp[i-1][j-1]);
-                    ans = Math.min(ans,dp[i][j]);
+                    matrix[i][j] = matrix[i][j] + Math.min(matrix[i-1][j],matrix[i-1][j-1]);
+                    ans = Math.min(ans,matrix[i][j]);
                 }else{
-                    dp[i][j] = matrix[i][j] + Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i-1][j+1]));
-                    ans = Math.min(ans,dp[i][j]);
+                    matrix[i][j] = matrix[i][j] + Math.min(matrix[i-1][j],Math.min(matrix[i-1][j-1],matrix[i-1][j+1]));
+                    ans = Math.min(ans,matrix[i][j]);
                 }
             }
         }
