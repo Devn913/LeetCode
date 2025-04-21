5102d51b90127f63fdc71463495c52258fb0edc9
Dev Nirwal <devnirwal16@gmail.com>
2025-04-21 13:27:01 +0530

    Time: 2 ms (79.09%), Space: 47.8 MB (33.69%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,20 +1,41 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length+1][grid[0].length+1];
-        for(int i = 0;i<grid[0].length;i++){
-            dp[grid.length][i] = 500000;
-        }
-        for(int i = 0;i<grid.length-1;i++){
-            dp[i][grid[0].length] = 500000;
-        }
+    // Approach 1: Recursion - TLE
+    // TC        : O(2^(mxn))
+    // SC        : O(1) 
+    // public int helper(int[][] grid, int row, int col){
+    //     if(row == grid.length -1 && col ==  grid[0].length -1) return grid[row][col];
+    //     if(row>=grid.length || col>= grid[0].length) return Integer.MAX_VALUE;
 
-        for(int i = grid.length-1;i!=-1;i--){
-            for(int j = grid[0].length-1;j!=-1;j--){
-                dp[i][j] = grid[i][j] + Math.min(dp[i+1][j],dp[i][j+1]);
+    //     int choice1 = helper(grid,row+1,col);
+    //     int choice2 = helper(grid, row, col+1);
+
+    //     return Math.min(choice1,choice2) + grid[row][col];
+    // }
+    // public int minPathSum(int[][] grid) {
+    //     return helper(grid,0,0);
+    // }
+
+    // Approach 2: Memoization
+
+    public int helper(int[][] grid, int[][] memo, int row, int col){
+        if(row == grid.length -1 && col ==  grid[0].length -1) return grid[row][col];
+        if(row>=grid.length || col>= grid[0].length) return Integer.MAX_VALUE;
+        if(memo[row][col]!=-1) return memo[row][col];
+
+        int choice1 = helper(grid,memo, row+1, col);
+        int choice2 = helper(grid,memo, row,   col+1);
+
+        memo[row][col] = Math.min(choice1,choice2) + grid[row][col];
+        return memo[row][col];
+    }
+
+    public int minPathSum(int[][] grid) {
+        int[][] memo = new int[grid.length][grid[0].length];
+        for(int i = 0;i<grid.length;i++){
+            for(int j = 0;j<grid[0].length;j++){
+                memo[i][j] = -1;
             }
         }
-
-
-        return dp[0][0];
+        return helper(grid,memo,0,0);
     }
 }
