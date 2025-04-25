02e824b5623ebab181273e95a08f09eb25230288
Dev Nirwal <devnirwal16@gmail.com>
2025-04-25 16:57:59 +0530

    Time: 1 ms (85.08%), Space: 42.1 MB (40.49%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,21 +1,31 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-       List<List<Integer>> result = new ArrayList<>();
-       // handling base case
-       List<Integer> one = new ArrayList<>();
-       one.add(1);
-       result.add(one);
-       if(numRows==1) return result;
-        
-       for(int i = 1;i<numRows;i++){
-            List<Integer> toAdd = new ArrayList<>();
-            toAdd.add(1);
-            for(int j = 1;j<result.get(i-1).size();j++){
-                toAdd.add(result.get(i-1).get(j-1) + result.get(i-1).get(j));
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> one = new ArrayList<>();
+        one.add(1);
+        result.add(one);
+
+        if(numRows == 1) return result;
+
+        List<Integer> two = new ArrayList<>();
+        two.add(1);
+        two.add(1);
+        result.add(two);
+
+
+        if(numRows == 2) return result;
+
+
+        for(int i = 2;i<numRows;i++){
+            List<Integer> prev = result.get(i-1);
+            List<Integer> next = new ArrayList<>();
+            next.add(1);
+            for(int j = 1;j<prev.size();j++){
+                next.add(prev.get(j-1)+ prev.get(j));
             }
-            toAdd.add(1);
-            result.add(toAdd);
-       }
-       return result;
+            next.add(1);
+            result.add(next);
+        }
+        return result;
     }
 }
