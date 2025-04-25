e50ee7b66af549c5746dc370717b6f5be389025e
Dev Nirwal <devnirwal16@gmail.com>
2025-04-25 09:48:27 +0530

    Time: 10 ms (23.49%), Space: 46 MB (99.69%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,34 +2,39 @@ class Solution {
     class Pair{
         int start;
         int end;
-        
-        Pair(int s, int e){
-            this.start = s;
-            this.end   = e;
+        Pair(int start, int end){
+            this.start = start;
+            this.end   = end; 
         }
     }
     public int[][] merge(int[][] intervals) {
         if(intervals.length<2) return intervals;
+
         Arrays.sort(intervals,(a,b)-> Integer.compare(a[0],b[0]));
-        ArrayList<Pair> res = new ArrayList<>();
-        int i = 0;
-        while(i<intervals.length){
-            int currStart = intervals[i][0];
-            int currEnd       = intervals[i][1];
-            i++;
-            while(i<intervals.length && currEnd>=intervals[i][0]){
+        ArrayList<Pair> list = new ArrayList<>();
 
-                currEnd = Math.max(intervals[i][1],currEnd);
-                i++;
+        int index = 0;
+        while(index<intervals.length){
+            int currStart = intervals[index][0];
+            int currEnd   = intervals[index][1];
+            index++;
+
+            while(index<intervals.length && currEnd>=intervals[index][0]){
+                currEnd = Math.max(currEnd,intervals[index][1]);
+                
+                index++;
             }
-            res.add(new Pair(currStart,currEnd));
-        }
 
-        int[][] ans = new int[res.size()][2];
-        for(int j = 0;j<ans.length;j++){
-            ans[j][0] = res.get(j).start;
-            ans[j][1] = res.get(j).end;
+            list.add(new Pair(currStart,currEnd));
+
         }
-        return ans;
+        int[][] res = new int[list.size()][2];
+        for(int  i =0;i<res.length;i++){
+            res[i][0] = list.get(i).start;
+            res[i][1]  = list.get(i).end;
+        }
+        return res;
+
+
     }
 }
