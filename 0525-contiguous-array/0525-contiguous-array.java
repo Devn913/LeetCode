db615f5480182220f7526e7bfe381ad015fd201b
Dev Nirwal <devnirwal16@gmail.com>
2025-04-28 13:23:47 +0530

    Time: 23 ms (85.81%), Space: 55.4 MB (50.41%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -1,19 +1,19 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        int ans = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-
         int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int ans = 0;
         map.put(0,-1);
-        for(int i= 0;i<nums.length;i++){
-            if(nums[i] == 1){
-                count++;
-            }else{
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] == 0){
                 count--;
+            }else{
+                count++;
             }
 
+
             if(map.containsKey(count)){
-                ans = Math.max(ans,i-map.get(count));
+                ans = Math.max(ans, i - map.get(count));
             }else{
                 map.put(count,i);
             }
