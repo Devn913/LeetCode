d3154cfe3a7acc50071b50888dc988effa28a757
Dev Nirwal <devnirwal16@gmail.com>
2025-04-28 10:00:53 +0530

    Time: 0 ms (100%), Space: 41.1 MB (92.83%) - LeetHub

--- 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.java
+++ 0988-flip-equivalent-binary-trees/0988-flip-equivalent-binary-trees.java
@@ -15,12 +15,12 @@
  */
 class Solution {
     public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2== null) return true;
+        if(root1==null && root2== null) return true;
         if(root1==null || root2 == null) return false;
 
-        if(root1.val!=root2.val) return false; 
-        return (flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left)) ||
-        (flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right));
+        if(root1.val!=root2.val) return false;
 
+        return (flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left)) || 
+                (flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right)) ;
     }
 }
