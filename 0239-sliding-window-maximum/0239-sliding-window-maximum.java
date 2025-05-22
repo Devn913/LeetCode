abb81d40dc32592d599ca7ffcfbb43025e9dfd27
Dev Nirwal <devnirwal16@gmail.com>
2025-05-22 13:19:18 +0530

    Time: 32 ms (54.03%), Space: 58.4 MB (83.56%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,18 +1,15 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        TreeMap<Integer,Integer> tm = new TreeMap<>();
-        for(int i = 0;i<k-1;i++){
-            tm.put(nums[i],tm.getOrDefault(nums[i],0)+1);
+        int n = nums.length;
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] result = new int[n-k+1];
+        int currentIndex = 0;
+        for(int i = 0;i<nums.length;i++){
+            while(!dq.isEmpty() && dq.peekFirst()<=i-k) dq.pollFirst();
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
+            dq.offerLast(i);
+            if(i>=k-1) result[currentIndex++] = nums[dq.peekFirst()];
         }
-        int[] ans = new int[nums.length - k + 1];
-        int start = -1;
-        while(start+k<nums.length){
-            tm.put(nums[start+k],tm.getOrDefault(nums[start+k],0)+1);
-            start++;
-            ans[start] = tm.lastKey();
-            if(tm.get(nums[start]) == 1) tm.remove(nums[start]);
-            else tm.put(nums[start],tm.get(nums[start])-1);
-        }
-        return ans;
+        return result;
     }
 }
