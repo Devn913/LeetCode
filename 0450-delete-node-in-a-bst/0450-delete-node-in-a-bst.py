ec0ed5feed055055f5cb013dedc43d0823bac501
Dev Nirwal <devnirwal16@gmail.com>
2025-11-25 10:13:08 +0530

    Time: 0 ms (100%), Space: 21.1 MB (99.54%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,37 +5,34 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
-    
-
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        def getInoderSucc(root):
-        
-            while(root.right is not None):
-                root = root.right
-            return root
-        
-        def getInorderPrec(root):
-
-            while(root.left is not None):
+        def getSucc(root):
+            while(root.left!=None):
                 root = root.left
             return root
-
-        if(root is None):
+        if(root == None):
             return None
-        
-        if(root.val<key):
-            root.right = self.deleteNode(root.right,key)
-        elif(root.val>key):
+
+        if(root.val >key):
             root.left = self.deleteNode(root.left,key)
+        elif(root.val<key):
+            root.right = self.deleteNode(root.right,key)
         else:
+            if(root.left == None and root.right == None):
+                return None
             if(root.left == None):
                 return root.right
-            elif(root.right == None):
+            if(root.right == None):
                 return root.left
             
-            # node = getInoderSucc(root.left)
-            node = getInorderPrec(root.right)
+            node = getSucc(root.right)
             root.val = node.val
             root.right = self.deleteNode(root.right,node.val)
         return root
+        
+            
+
+
+
+
+        
