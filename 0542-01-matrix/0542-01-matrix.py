c3d4f9a66ec50e3d11e700628029ff3c0752dd19
Dev Nirwal <devnirwal16@gmail.com>
2025-12-02 11:36:20 +0530

    Time: 117 ms (79.43%), Space: 21.9 MB (14.18%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,16 +2,16 @@ class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         n = len(mat)
         m = len(mat[0])
-        q = deque()
         vis = [[False for _ in range(m)] for _ in range(n)]
         ans = [[-1 for _ in range(m)] for _ in range(n)]
+        q = deque()
 
         for i in range(n):
             for j in range(m):
                 if(mat[i][j] == 0):
-                    q.append((i,j,0))
                     vis[i][j] = True
-        dirs = [[0,1],[1,0],[0,-1],[-1,0]]
+                    q.append((i,j,0))
+        dirs = [[0,-1],[0,1],[1,0],[-1,0]]
         while q:
             x,y,dist = q.popleft()
             ans[x][y] = dist
@@ -19,9 +19,6 @@ class Solution:
                 nx = x+dx
                 ny = y+dy
                 if(0<=nx<n and 0<=ny<m and not vis[nx][ny]):
-                    q.append((nx,ny,1+dist))
+                    q.append((nx,ny, 1+ dist))
                     vis[nx][ny] = True
-
         return ans
-
-
